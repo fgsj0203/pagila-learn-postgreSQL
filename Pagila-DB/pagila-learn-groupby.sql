2ba3dfc917a85ad1d584e3605213fc76d67cd80b
Francisco Junior <fgsj.dev@outlook.com>
2025-07-24 15:47:22 -0300

    exercise 33: return movies for category

--- Pagila-DB/pagila-learn-groupby.sql
+++ Pagila-DB/pagila-learn-groupby.sql
@@ -1,5 +1,12 @@
+-- Exercise 31: return exists clients for country
 select COUNT(*), cty.country
 from customer as c inner join address as ad on c.address_id = ad.address_id
 inner join city as cy on cy.city_id = ad.city_id
 inner join country as cty on cty.country_id = cy.country_id
 group by cty.country
+
+-- Exercise 32: return movies for category
+select COUNT(*) as "Quantidade de filmes na categoria", c."name"
+from film as f inner join film_category as fc on f.film_id = fc.film_id
+inner join category as c on c.category_id = fc.category_id
+group by c."name"
