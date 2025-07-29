0766ce25dc7ae443b1469da8ef27b1bdc191979a
Francisco Junior <fgsj.dev@outlook.com>
2025-07-29 16:10:52 -0300

    update: change order problemas/solutions

--- Pagila-DB/pagila-learn-others-content.sql
+++ Pagila-DB/pagila-learn-others-content.sql
@@ -6,18 +6,21 @@ City: Recife-PE
 Description: Queries solutions of categories others, categorie of mixe content of level between beginner and intermediary
 """
 
--- Exercise 62: return movies of language is "English"
-select *, l."name"
-from film as f inner join "language" as l on f.language_id = l.language_id
-where l."name" = 'English'
-----------------------------------------------------------
-
 -- Exercise 61: return all languages of movies
 select l."name" as "All languages of movies"
 from "language" as l
 group by l."name"
 --(optional)
 order by l."name" asc -- Order values asceding using names
+----------------------------------------------------------
+
+-- Exercise 62: return movies of language is "English"
+select *, l."name"
+from film as f inner join "language" as l on f.language_id = l.language_id
+where l."name" = 'English'
+----------------------------------------------------------
+
+
 
 
 
