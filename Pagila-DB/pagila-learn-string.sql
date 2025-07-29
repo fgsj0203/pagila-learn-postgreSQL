a87d6e385de8e6f6f183c03c7a0ffd578cb98b48
Francisco Junior <fgsj.dev@outlook.com>
2025-07-29 14:18:51 -0300

    first commit exercises of string query

---
+++ Pagila-DB/pagila-learn-string.sql
@@ -0,0 +1,30 @@
+-- Consultas com manipulação de Strings
+
+-- Exercise 51: return data of title film contain word "LOVE"
+select *
+from film as f
+where f.title LIKE '%LOVE%'
+--------------------------------------------------------------
+
+
+-- Exercise 52: return data with name contain word "SON" in end
+select *
+from customer as c
+where c.last_name LIKE '%SON'
+--------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
