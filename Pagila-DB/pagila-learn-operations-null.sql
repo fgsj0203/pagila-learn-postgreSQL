5d16070f460c42a5f0a6b2079fbff283aa4f29d6
Francisco Junior <fgsj.dev@outlook.com>
2025-07-29 14:54:32 -0300

    update: change order of exercises

--- Pagila-DB/pagila-learn-operations-null.sql
+++ Pagila-DB/pagila-learn-operations-null.sql
@@ -1,11 +1,12 @@
+-- Exercise 56: verify if values null in column "return_date"
+select *
+from rental as r
+where r.return_date IS NULL
+-----------------------------------------------------------------
+
 -- Exercise 58: return all customers that field email is not null
 select *
 from customer as c
 where c.email IS NOT NULL
 -----------------------------------------------------------------
 
--- Exercise 56: verify if values null in column "return_date"
-select *
-from rental as r
-where r.return_date IS NULL
------------------------------------------------------------------
